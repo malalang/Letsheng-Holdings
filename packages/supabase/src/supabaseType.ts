fbc031794f2db0b6a1124799d6f00db2fdac3bf9
Abram Elton Ntsako <malalangptyltd@gmail.com>
2026-06-15 12:23:28 +0200

    fix(letsheng): add is_admin function to Supabase types

--- packages/supabase/src/supabaseType.ts
+++ packages/supabase/src/supabaseType.ts
@@ -322,7 +322,10 @@ export type Database = {
       [_ in never]: never;
     };
     Functions: {
-      [_ in never]: never;
+      is_admin: {
+        Args: Record<string, never>;
+        Returns: boolean;
+      };
     };
     Enums: {
       [_ in never]: never;
